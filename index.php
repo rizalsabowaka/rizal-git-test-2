29566bb9f71bced09e038e4b9cea2300d4832dc7
rizalsabowaka <laodemurizal7@gmail.com>
2025-06-28 03:08:17 +0800

    menambahkan li di index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quibusdam culpa velit quisquam perferendis, fuga possimus vel nobis sapiente magni assumenda deleniti ipsum eos magnam? Rerum iure minus delectus? Incidunt!</p>
+    <li>List 1</li>
 </body>
 </html>
